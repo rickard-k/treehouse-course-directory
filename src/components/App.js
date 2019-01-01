072a42a9ec5f6a0b84e081b8cf2440e336d4b260
rickard-k <rick137@gmail.com>
2019-01-01 14:13:31 +0100

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
+import NotFound from './NotFound'
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/teachers" component={Teachers}></Route>
+        <Route path="/courses" component={Courses}></Route>
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
